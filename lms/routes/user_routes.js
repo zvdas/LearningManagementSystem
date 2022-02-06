2dc26e248f136608641c0938c228cec4021a8107
zvdas <rodriguezzude@gmail.com>
2022-02-06 23:11:20 +0530

    create static views

--- lms/routes/user_routes.js
+++ lms/routes/user_routes.js
@@ -4,8 +4,16 @@ const path = require('path');
 
 const router = express.Router();
 
-router.get('/', (req,res)=>{
+router.get('/home', (req,res)=>{
     res.sendFile(path.join(__dirname,'../','views','home.html'));
+});
+
+router.get('/navbar', (req,res)=>{
+    res.sendFile(path.join(__dirname,'../','views','navbar.html'));
+});
+
+router.get('/edit', (req,res)=>{
+    res.sendFile(path.join(__dirname,'../','views','user_edit.html'));
 });
 
 module.exports = router;
