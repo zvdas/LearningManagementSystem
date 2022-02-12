73cac6f8ad88ef3bd68536b76d4a843ad0a30c5e
zvdas <rodriguezzude@gmail.com>
2022-02-12 14:56:37 +0530

    views_static

--- lms/routes/course_routes.js
+++ lms/routes/course_routes.js
@@ -4,8 +4,20 @@ const path = require('path');
 
 const router = express.Router();
 
-router.get('/', (req,res)=>{
+router.get('/new', (req,res)=>{
     res.sendFile(path.join(__dirname,'../','views','course_new.html'));
+});
+
+router.get('/dashboard', (req,res)=>{
+    res.sendFile(path.join(__dirname,'../','views','dashboard.html'));
+});
+
+router.get('/update', (req,res)=>{
+    res.sendFile(path.join(__dirname,'../','views','course_update.html'));
+});
+
+router.get('/delete', (req,res)=>{
+    res.sendFile(path.join(__dirname,'../','views','course_delete.html'));
 });
 
 module.exports = router;
