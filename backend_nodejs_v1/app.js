cc31cd1f45912bd1baffea8af9d074ca456dccf6
zvdas <rodriguezzude@gmail.com>
2023-01-19 12:05:30 +0530

    changes to app.js

--- backend_nodejs_v1/app.js
+++ backend_nodejs_v1/app.js
@@ -50,6 +50,8 @@ app.use('/', (req, res) => {
     res.redirect('/user/new');
 })
 
-app.listen(process.env.PORT || 3000, ()=>{
-    console.log("Server is listening on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
 });
